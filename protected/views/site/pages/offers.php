c8daca8b2dc14c6e3217bafe564207f870834d6d
Araquach <adam@contra-design.com>
2013-01-10 13:26:33 +0000

    Updated and launched offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -4,27 +4,35 @@
 $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 
 ?>
-<!--
+
 <section id="offersMain">
 
 <h2>Special Offers</h2>
 
 <ul id="offerlist">
+
 <li id="offer1"><h3>Weekday Colour &amp; Cut Package</h3>
 <p>for just &pound;60</p>
-<p>with any of our Stylists</p>
-<p class="small">offer extended until 30/11/12<br>
-  For New Clients - not with any other offer - excludes Saturday</p></a>
+<p>with any of our Senior Stylists</p>
+<p class="small">until 15/02/13<br>
+  For New Clients - not with any other offer - excludes Saturday - Skin Test required 48 hours prior to colour services</p></a>
 </li>
 
-<li id="offer2">
+<li id="offer2"><h3>Weekday Colour &amp; Cut Package</h3>
+<p>for just &pound;40</p>
+<p>with any of our Graduate Stylists</p>
+<p class="small">offer until 15/02/13<br>
+  For New Clients - not with any other offer - excludes Saturday - Skin Test required 48 hours prior to colour services</p></a>
+</li>
+
+<li id="offer3">
   <h3>FREE product worth up to &pound;10</h3>
   <p>with every Cut, Dry &amp; Style<br>
-    Book in before the end of October for a Cut, Dry &amp; Style to receive a £10 product voucher absolutely FREE!</p>
-    <p class="small">extended until 30/11/12<br>
+    Book in before the end of January for a Cut, Dry &amp; Style to receive a £10 product voucher absolutely FREE!</p>
+    <p class="small">until 31/01/13<br>
     For New Clients - not with any other offer. Excludes Saturday</p></a>
 </li>
-
+<!--
 <li id="offer3"><a href="news.php#Tash">
   <h3>A Cut &amp; Colour for just &pound;25</h3>
   <p>with our soon-to-be qualified trainee stylist</p><p class="small">until 29/06/12<br>
@@ -34,15 +42,14 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
   <h3>FREE Cut, Dry &amp; Style</h3>
   <p>with every Kebelo System Treatment</p><p class="small">extended until 30/11/12<br>
     not with any other offer<br>Excludes Saturday</p></a></li>
-    
+  -->  
  </ul>  
  
  </section> 
  
- --> 
- 
+ <!--
  <section id="no_offer">
-  </section> <!--homeimage-->
+  </section> 
  <section id="no_offercopy">
  <h2>Special Offers</h2>
  <p>Look out for some great offers in the New Year!</p>
