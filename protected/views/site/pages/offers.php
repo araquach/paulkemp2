b16b042f610bbd3072c991af2ad60bf09af0b25a
Araquach <adam@contra-design.com>
2013-01-31 18:11:41 +0000

    Extended offer till end of feb

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -14,17 +14,17 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 <li id="offer1"><h3>Weekday Colour &amp; Cut Package</h3>
 <p>for just &pound;60</p>
 <p>with any of our Senior Stylists</p>
-<p class="small">until 15/02/13<br>
+<p class="small">until 28/02/13<br>
   For New Clients - not with any other offer - excludes Saturday - Skin Test required 48 hours prior to colour services</p></a>
 </li>
 
 <li id="offer2"><h3>Weekday Colour &amp; Cut Package</h3>
 <p>for just &pound;40</p>
 <p>with any of our Graduate Stylists</p>
-<p class="small">offer until 15/02/13<br>
+<p class="small">offer until 28/02/13<br>
   For New Clients - not with any other offer - excludes Saturday - Skin Test required 48 hours prior to colour services</p></a>
 </li>
-
+<!--
 <li id="offer3">
   <h3>FREE product worth up to &pound;10</h3>
   <p>with every Cut, Dry &amp; Style<br>
@@ -32,7 +32,7 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
     <p class="small">until 31/01/13<br>
     For New Clients - not with any other offer. Excludes Saturday</p></a>
 </li>
-<!--
+
 <li id="offer3"><a href="news.php#Tash">
   <h3>A Cut &amp; Colour for just &pound;25</h3>
   <p>with our soon-to-be qualified trainee stylist</p><p class="small">until 29/06/12<br>
