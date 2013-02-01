a388ec6d3a6d162eb10f25694adc6e268381c7ce
Araquach <adam@contra-design.com>
2013-02-01 15:12:20 +0000

    Introduce ZHtml and competition page

---
+++ protected/views/site/pages/competition.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $this SiteController */
+$this->pageTitle=Yii::app()->name . ' - Competitions - Hairdressers in Cheshire';
+?>
+
+
+<section id="competition">
+<div id="competition_copy">
+<p><strong>Ensure you look your very best this Valentines Day &hellip;</strong></p>
+<h1>WIN A SET OF GHD GOLD STYLING IRONS</h1>
+<p>We’re giving away a set of the  most desirable styling irons available! (worth £120) plus 3 runners up will receive complimentary Blow Dry’s for their big Valentines night out!</p>
+<?php echo CHtml::link('CLICK HERE to enter',array('valentine/create')); ?>
+
+
+
+</div> <!--end competition_copy-->
+
+
+</section> <!--end competition-->
