a670c6176e8e966d9693dd262407ada363ff55b2
Araquach <adam@contra-design.com>
2013-02-15 09:46:51 +0000

    changed layout slightly

--- protected/views/site/pages/reborn.php
+++ protected/views/site/pages/reborn.php
@@ -12,20 +12,20 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
  <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/home/hair_reborn_package.png" alt="Hair Reborn Package" width="321" height="107"/>
  <p>With a Senior Stylist</p>
  <p class="reborn_large">Worth &pound;150</p>
- <p>for only &pound;75</p>
+ <p class="reborn_med">for only &pound;75</p>
  <br>
  <p>with a Stylist - only &pound;65</p>
  <p class="reborn_small">worth over £130</p>
  <br>
  <p><em>Package includes:</em></p>
- <p class="package">Colour & Cut<br>
+ <p class="package">Colour &amp; Cut<br>
  In-Salon Hair Reborn Treatment<br>
  FREE Hair Reborn Aftercare<br>
  (product pack worth over £40)</p>
  <p class="package reborn_small">New clients only, not with any other offer</p>
  
  
- <?php echo CHtml::link('Find out more here',array('site/page','view'=>'reborn')); ?>
+<p class="link"><?php echo CHtml::link('Find out more here',array('site/page','view'=>'reborn')); ?></p>
   
  </section> <!--#reborn-->
  </div> <!--#reborn_bground-->
