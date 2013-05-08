26921cfc801b5d6d03f05061b6b0f8536cc56715
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-05-08 16:23:02 +0100

    added graduate offer page

---
+++ protected/views/site/pages/graduate.php
@@ -0,0 +1,28 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Graduate Stylist Offer - Hairdressers in Cheshire';
+
+?>
+
+<section id="graduate">
+
+	<section id="graduatecopy">
+    <h2>Megan now taking bookings</h2>
+    <p>Megan Mullaney joined the Paul Kemp Team in July 2012 as an apprentice and has recently been promoted to a Graduate Stylist.</p>
+    <p>The training to get to this level is intensive but extremely rewarding, with Megan now fully skilled in all aspects of hairdressing - it's now just a case of her practicing and polishing up her skills.</p>
+    <p>Megan is eager to build up a strong client base so she can quickly progress through the ranks towards being a Senior Stylist.</p>
+    <p><em>In order to help her build we've launched an unbeatable offer that we'd love you to take advantage of:</em></p>
+    
+    <p class="offer">Colour &amp; Cut with Megan for just &pound;30</p>
+    <p class="offer">A Cut, Dry &amp; Style for just &pound;15</p>
+    <p class="offer">A blow dry or hair up for just &pound;7.50</p>
+    
+    
+    
+  	<p class="small">(one offer per person, not with any other offer<br>excludes Saturday's, ends 30/06/13)</p>
+    
+</section> <!--end graduatecopy-->
+
+</section> <!--end graduate-->
+
