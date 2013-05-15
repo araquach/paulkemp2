f40eeb750e58f4f45b970c35fb7c317421d64e5a
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-05-15 10:46:47 +0100

    working on offers page

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -6,36 +6,21 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 ?>
 
 <section id="offersMain">
-<!--
-<h2>Special Offers</h2>
 
-<ul id="offerlist">
+<h2>Special Offer</h2>
 
-
-<li id="offer3">
-  <h3>FREE product worth up to &pound;10</h3>
-  <p>with every Cut, Dry &amp; Style<br>
-    Book in before the end of January for a Cut, Dry &amp; Style to receive a £10 product voucher absolutely FREE!</p>
-    <p class="small">until 31/01/13<br>
-    For New Clients - not with any other offer. Excludes Saturday</p></a>
+<li id="offer1">
+  <h3>FREE Colour with every <br>Cut, Dry &amp; Style</h3>
+  <p>For new clients throughout May<br>
+    </p>
+    <p class="small">until 29/06/13<br>
+    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</p></a>
 </li>
-
-<li id="offer3"><a href="news.php#Tash">
-  <h3>A Cut &amp; Colour for just &pound;25</h3>
-  <p>with our soon-to-be qualified trainee stylist</p><p class="small">until 29/06/12<br>
-    For New clients - not with any other offer<br>Excludes Saturday</p></a></li>
-
-<li id="offer4"><a href="kebelo">
-  <h3>FREE Cut, Dry &amp; Style</h3>
-  <p>with every Kebelo System Treatment</p><p class="small">extended until 30/11/12<br>
-    not with any other offer<br>Excludes Saturday</p></a></li>
-  
- </ul>  
  
- </section> 
-  --> 
+</section> <!--#offersMain-->
  
- <section id="no_offer">
+ 
+ <!--<section id="no_offer">
   </section> 
  <section id="no_offercopy">
  <h2>Special Offers</h2>
@@ -43,6 +28,6 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
  <p>
  	<a href="http://www.facebook.com/PaulKempHairdressing" target="_blank">Visit our Facebook page to keep up to date with the latest offers</a>
  </p>
- </section> <!--offercopy-->
+ </section> <!--offercopy--*>-->
 
   
