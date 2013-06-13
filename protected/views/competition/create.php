2ea9bcc983bc3d6fcf9889e1ffbbd472df58bc7b
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-06-14 00:24:49 +0100

    made the suitable changes

--- protected/views/competition/create.php
+++ protected/views/competition/create.php
@@ -14,10 +14,12 @@
 
 <div id="competition_copy">
 <!--<p class="big comp_end">The competition is now closed - look out for another one soon!</p>-->
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/valentines_logo.gif" alt="Valentines" width="298" height="197"/>
-<a href="http://mrlaus.com" target="_blank" class="link_image"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/mr_laus.gif" alt="Valentines" width="320" height="85"/></a>
-<p>For your chance to win this amazing prize, just answer the following question:</p>
-<p class="question">Where is Mr Lau's Restaurant located?</p>
+
+<h1>WIN</h1>
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/ghdlogo.png" alt="GHD" width="200" height="134"/>
+<p class="candy">Candy Collection<br>Styling Irons</p>
+<p>For your chance to win a set of GHD Candy Collection Styling Irons just answer the following question:</p>
+<p class="question">Wht are GHD's so damn good?</p>
 
 
 <?php  echo $this->renderPartial('_form', array('model'=>$model)); ?>
