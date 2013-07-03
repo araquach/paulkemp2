d8a3f05772349cb65a5d37d156a74bafb0332315
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-07-03 13:18:06 +0100

    added katie offer page

---
+++ protected/views/site/pages/katie.php
@@ -0,0 +1,28 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Special Offer - Hairdressers in Cheshire';
+
+?>
+
+<section id="leaver">
+
+	<section id="leavercopy">
+    <h2>Katie Sergeant no longer at Paul Kemp Hairdressing</h2>
+    <p>In early July <em>Katie Sergeant</em> left Paul Kemp Hairdressing. She has decided to pursue a new opportunity that has come up. We were sorry to see her go.</p>
+    <p>If you were a regular client of Katie's we apologise for her quick departure and we hope it hasn't caused you any inconvenience.
+    <p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, all at a similar level to Katie.  We have the talented <em>Jack Broady</em> who joined the salon a while ago plus we can also highly recommend <em>Natasha Bailey</em> who is at the same level as Katie. We would also highly recommend our latest Graduate stylist <em>Megan Mullaney</em> who's prices are currently exceptional value as she's looking to build up her client base.</p>
+    <p>We would hate to lose you as a customer, we really appreciate your loyalty to the salon and we hope we can continue to look after you for the foreseeable future, so we have some fantastic offers as our way of apologising for the inconvenience&hellip;</p>
+    <p class="offer">We're offering a <strong>free</strong> colour with every Cut, Dry and Style plus a <strong>free £10 </strong>product voucher.</p>
+   
+    <p>Alternatively, if you just need a Cut, Dry and Style you will receive a <strong>£10 product voucher</strong> as our way to apologise for the inconvenience.</p>
+    
+    <p>offers apply to Jack, Leon and Tash for all previous regular clients to Nathan. Please mention the offer to your stylist.</p> <p class="small">(one offer per person, not transferable, ends 31/10/12)</p>
+    
+    <p><em>Pictured right (top to bottom)</em></p>
+    <p><em>Jack Broady, Leon Pritchard &amp; Natasha Bailey</em></p>
+    
+</section> <!--end leavercopy-->
+
+</section> <!--end leaver-->
+
