7d1a6566ea77aa9ec277b96add2aa56404bb1bd7
Araquach <adam@contra-design.com>
2013-07-11 18:15:25 +0100

    updated home image/page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,18 +8,11 @@ $this->pageTitle=Yii::app()->name;
 <!--Main bit-->
     </section>
     <section id="homecopy">
-    
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logos/hair_reborn.png" alt="TIGI Hair Reborn" width="417" height="368"/>
-    
-    <p id="reputation">Paul Kemp Hairdressing is excited to introduce this incredible range of products available exclusively in the salon</p>
-    <?php echo CHtml::link('Find out more about the range here',array('site/page','view'=>'news', '#'=>'HairReborn')); ?>
-    </section>
-    <section id="home_copy_hidden">
     <h2>A New Standard of Hairdressing</h2>
     <p>Paul Kemp Hairdressing is located right in the heart of Warrington town centre. Sister salon to the award winning <em><a href="http://www.jakatasalon.co.uk/" target="_blank">Jakata</a></em>, the stunning salon opened in June 2011 with the aim to offer an ultra relaxing atmosphere, first class customer service, alongside the highest level of hairdressing expertise.</p>
     <p>The talented hairdressing team are all trained to the highest level in cutting, colouring and styling hair, with specialists in technical colour, hair straightening, wedding hair and hair extensions. The team has a wealth of experience in all aspects of hairdressing so you can just sit back, relax and enjoy the experience.</p>
-    <p>The salon has built up a strong reputation for creating stunning, fashionable looks tailored to suit you, and your lifestyle, so if you're looking for a new <strong class="paraend">hairdressers in Warrington</strong> then look no further!</p>
-	</section> 
+    <p><em>The salon has built up a strong reputation for creating stunning, fashionable looks tailored to suit you, and your lifestyle, so if you're looking for a new <strong class="paraend">hairdressers in Warrington</strong> then look no further!</em></p>
+	</section>
   
 <!--NewsItems-->
     <?php echo CHtml::link('<aside id="news1">
