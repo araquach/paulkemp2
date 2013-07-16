d17ef1a761c84c3de1e351861999ed59f47d1b2a
Araquach <adam@contra-design.com>
2013-07-16 16:47:12 +0100

    Added multiple types of styling to offers CSS

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -5,11 +5,11 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 
 ?>
 
-<!--<section id="offersMain">
+<section id="offersMain">
 
 <h2>Special Offer</h2>
 
-<div id="offer1">
+<!--<div id="offer1">
   <h3>FREE Colour <br>with every <br>Cut, Dry &amp; Style</h3>
   <p>For new clients <br>throughout June
     </p>
@@ -18,9 +18,46 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 </div>
  
 </section> <!--#offersMain--*>-->
+
+
+<section id="offersMain">
+
+<h2>Special Offers</h2>
+
+<ul id="offerlist">
+
+<li id="offer1"><h3>Weekday Colour &amp; Cut Package</h3>
+<p>for just &pound;60</p>
+<p>with any of our Stylists</p>
+<p class="small">offer extended until 30/11/12<br>
+  For New Clients - not with any other offer - excludes Saturday</p></a>
+</li>
+
+<li id="offer2">
+  <h3>FREE product worth up to &pound;10</h3>
+  <p>with every Cut, Dry &amp; Style<br>
+    Book in before the end of October for a Cut, Dry &amp; Style to receive a £10 product voucher absolutely FREE!</p>
+    <p class="small">extended until 30/11/12<br>
+    For New Clients - not with any other offer. Excludes Saturday</p></a>
+</li>
+
+<li id="offer3"><a href="news.php#Tash">
+  <h3>A Cut &amp; Colour for just &pound;25</h3>
+  <p>with our soon-to-be qualified trainee stylist</p><p class="small">until 29/06/12<br>
+    For New clients - not with any other offer<br>Excludes Saturday</p></a></li>
+
+<li id="offer4"><a href="kebelo">
+  <h3>FREE Cut, Dry &amp; Style</h3>
+  <p>with every Kebelo System Treatment</p><p class="small">extended until 30/11/12<br>
+    not with any other offer<br>Excludes Saturday</p></a></li>
+    
+ </ul>  
+ 
+ </section> 
+
  
  
- <section id="no_offer">
+ <!--<section id="no_offer">
   </section> 
  <section id="no_offercopy">
  <h2>Special Offers</h2>
@@ -28,6 +65,6 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
  <p>
  	<a href="http://www.facebook.com/PaulKempHairdressing" target="_blank">Visit our Facebook page to keep up to date with the latest offers</a>
  </p>
- </section> <!--offercopy-->
+ </section> <!--no_offer-->
 
  
