3b9da250495ce2c02e7fd37a253e7c75b28871d0
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-07-31 09:59:53 +0100

    Started on Michelle page

---
+++ protected/views/site/pages/michelle.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Michelle Railton now at PK - Hairdressers in Cheshire';
+
+?>
+
+<section id="michelle">
+
+	<section id="michelle_copy">
+	<h2>Welcoming Michelle Railton to the team</h2>
+	<p></p>
+	
+	</section>
+</section>
