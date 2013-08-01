7ea2425b5324270b1d0df56934b765d7ad4c5e94
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-08-01 13:35:19 +0100

    added images, did news and did Michelle age (almost)

--- protected/views/site/pages/michelle.php
+++ protected/views/site/pages/michelle.php
@@ -9,7 +9,19 @@ $this->pageTitle=Yii::app()->name . ' - Michelle Railton now at PK - Hairdresser
 
 	<section id="michelle_copy">
 	<h2>Welcoming Michelle Railton to the team</h2>
-	<p></p>
+	<p class="bold"><strong>We're excited to announce experienced stylist Michelle Railton is now a member of our team. Michelle, 25, from Warrington has been hairdressing for over 9 years.</strong></p>
+	<p>She previously worked at Daniel Anderson Hairdressing for many years before deciding setting up on her own. After a year she decided self employment wasn't for her and chose to take some time out from hairdressing.</p>
+	<p>After a brief time working in the recruitment industry she realised hairdressing was definitely where her heart was.
+	When she applied here for the role we snapped her up, as PK stylist <em>Leon Pritchard</em> (who had previously worked with Michelle) had only great things to say about her.</p>
+	<p>Michelle is skilled in all aspects of hairdressing including <strong>advanced cutting, colouring &amp; styling, hair ups plus men's hairdressing.</strong>
+	She particularly loves soft, natural &amp; luxurious cuts and colours, she's highly skilled in short cuts and loves men's hairdressing too (particularly indie/mod cuts).</p>
+	<p>Michelle said <em>"Being part of such a strong team is really exciting, I can't wait to build up a new client base, plus hopefully see some of my regulars from Daniel Anderson Hairdressing too!"</em></p>
+	<p class="offerHeading">Special Offers with Michelle</p>
+	<p class="offer">Cut &amp; Colour</p>
+	<p class="small">offer until 09/08/13<br>
+	  For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
+	
+	<?php echo CHtml::link('<p class="offer">Click here to find out more about Michelle&gt;</p>',array('site/page','view'=>'news', '#'=>'MichelleRailton')); ?>
 	
 	</section>
 </section>
