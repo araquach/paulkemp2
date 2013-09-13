8d0929ae230fa6a058e0d4224a11bb38ed969d05
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-09-13 17:22:45 +0100

    updated offer - need new imagery

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -5,51 +5,52 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 
 ?>
 
-<!--<div id="offer1">
-  <h3>FREE Colour <br>with every <br>Cut, Dry &amp; Style</h3>
-  <p>For new clients <br>throughout June
-    </p>
-    <small>until 29/06/13<br>
-    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small></a>
-</div>
- 
-</section> <!- -#offersMain- -*>-->
-
-
-<!--<section id="offersMain">
+<section id="offersMain">
 
 <h2>Special Offers</h2>
 
-<section id="offerlist">
+	<section id="offerlist">
+	<div id="offer1" class="offer">
+	  <h3>FREE Colour with every <br>Cut, Dry &amp; Style</h3>
+	  <p>For new clients <br>throughout the month</p>
+	    <p class="small">until 31/10/13<br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</div>
+	
+	<div id="offer2" class="offer">
+	  <h3>£15 Blow Dry <br>until 4pm</h3>
+	  <p>For new clients <br>throughout the month</p>
+	    <p class="small">until 31/10/13<br>
+	    For New Clients - not with any other offer. <br>Excludes Saturday and evenings</small>
+	</div>
+	</section> <!--offerlist--> 
 
-<div id="offer1" class="offer"><h3>Senior Stylist Colour &amp; Cut Package</h3>
+</section> <!--#offersMain-->
+
+<!--<div id="offer1" class="offer"><h3>Senior Stylist Colour &amp; Cut Package</h3>
 <p>for just &pound;60</p>
 <p>with any of our Senior Stylists</p>
 <p class="small">offer until <time datetime="2013-08-30">30/08/13</time><br>
   For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
-</div>
+</div>-->
 
-<div id="offer2" class="offer">
+<!--<div id="offer2" class="offer">
   <h3>Stylist Colour &amp; Cut Package</h3>
   <p>for just &pound;40<br>
     <p>with any of our Stylists</p>
     <p class="small">offer until <time datetime="2013-08-30">30/08/13</time><br>
       For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
- </div>
+ </div>-->
   
-<?php echo CHtml::link('<div id="offer4" class="offer">
+<?php /* echo CHtml::link('<div id="offer4" class="offer">
   <h3>FREE Cut, Dry &amp; Style</h3>
   <p>with every Kebelo System Treatment</p><p class="small">until <time datetime="2013-08-30">30/08/13</time><br>
     not with any other offer<br>Excludes Saturday</p>
- </div>', array('kebelo/advantage')); ?>
-    
- </section>   <!-offerlist-*> 
- 
- </section> <!-offersMain-*>-->
+ </div>', array('kebelo/advantage')); */ ?>
 
  
  
- <section id="no_offer">
+ <!--<section id="no_offer">
   </section> 
  <section id="no_offercopy">
  <h2>Special Offers</h2>
@@ -57,6 +58,6 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
  <p>
  	<a href="http://www.facebook.com/PaulKempHairdressing" target="_blank">Visit our Facebook page to keep up to date with all the latest news and offers</a>
  </p>
- </section> <!--no_offer-->
+ </section> <!- -no_offer- -*>-->
 
  
