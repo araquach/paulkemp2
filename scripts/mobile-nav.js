14d4ab82527b9606e27e0c626a61709221d7de01
Araquach <adam@contra-design.com>
2013-10-05 17:28:13 +0100

    started doing the work

---
+++ scripts/mobile-nav.js
@@ -0,0 +1,11 @@
+jQuery.noConflict();
+(function($){
+	$(document).ready(function(){
+		var button = '<a href="#" class="button"></a>';
+		$('nav').parent().append(button);
+		$('a.button').click(function(){
+			$('nav ul').slideToggle();
+		return false;
+		}); // end click
+	});
+})(jQuery);
