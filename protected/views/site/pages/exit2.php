f6381672537e98ac9779283eb3677d73883c37e9
Araquach <adam@contra-design.com>
2013-10-09 18:11:47 +0100

    Adapted Paul offer

--- protected/views/site/pages/exit2.php
+++ protected/views/site/pages/exit2.php
@@ -10,12 +10,12 @@ $this->pageTitle=Yii::app()->name . ' - Special Offer - Hairdressers in Cheshire
 	<section id="leavercopy">
     <h2>Paul Kemp no longer employed at the salon</h2>
     <p>As a regular client of Paul's we thought we had best inform you that he is no longer an employee at the salon (if you didn't already know). He resigned last month and has decided to pursue a different career opportunity.</p>
-    <p>Salon owner Adam Carter said "It's business as usual at PK - the team are stronger than ever. <br> We can't apologise enough for Paul's quick departure and any inconvenience it may have caused."</p>
+    <p>Salon owner Adam Carter said "It's business as usual at PK - the team are stronger than ever. <br> We can't apologise enough for his quick departure and any inconvenience it may have caused."</p>
     <p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, equally as talented as Paul.  We have salon team leader <em>Kellie Reedy</em> along with <em>Isobelle Lamb</em> and <em>Jo Mahoney</em>, all well established stylists in the salon.  If you prefer a man to do your hair then <em>Leon Pritchard</em> and <em>Jack Broady</em> are both fantastic stylists and would love to look after you. Michelle Railton also recently joined the team - she has a wealth of experience and is eager to build her client base.</p>
-    <p><em>We would hate to lose you as a customer, we hope we can continue to look after you for the foreseeable future. We have a great offer as our way of apologising for the inconvenience&hellip;</em></p>
+    <p><em>We would hate to lose you as a customer, we hope we can continue to look after you for the foreseeable future. We have a special offer as our way of apologising for the inconvenience&hellip;</em></p>
   	<p class="small">Please note, many of the stylists prime appointment slots get taken quickly. Please allow plenty of time to ensure you get an appointment with the stylist of your choice</p>
-    <p class="offer">We're offering <strong>10% off</strong> your next visit</p>
-    <p>offer available with all PK team members.</p> <p class="small">(one offer per person, not transferable, ends 30/11/13)</p>
+    <p class="offer">We're offering you <strong>15% off</strong> your next visit</p>
+    <p>offer available with all PK team members for clients of Paul.</p> <p class="small">(one offer per person, not transferable, ends 30/11/13)</p>
     
 	    <p><em>Pictured right (clockwise from top)</em></p>
     <p><em>Kellie Reedy, Isobelle Lamb, Leon Pritchard, Michelle Railton, Jack Broady &amp; Jo Mahoney  </em></p>
