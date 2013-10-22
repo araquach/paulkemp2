fa1c0b16c050879b6b443fa80bfd4357db23d5cf
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-10-22 20:24:42 +0100

    done some styling

--- protected/views/offer/index.php
+++ protected/views/offer/index.php
@@ -4,7 +4,7 @@
 
 <div id="limited">
 <div id="limited_copy">
-<h1>Special Offer for <br><?php echo ucfirst($model->first_name) . ' ' . ucfirst($model->last_name);?></h1>
+<h1><strong>Special Offer for <br><?php echo ucfirst($model->first_name) . ' ' . ucfirst($model->last_name);?></strong></h1>
 
 <p>Hi <?php echo ucfirst($model->first_name); ?>,</p>
 
@@ -13,36 +13,38 @@
 <?php elseif ($model->number_visits == 1): ?>
 <?php echo '<p>As you\'ve only experienced the salon once before we would love you to take advantage of this fantastic offer:</p>';?>
 <?php else: ?>
-<?php echo '<p>We have specially selected you to take advantage of this fantastic limited offer:</p>';?>
+<?php echo '<p>You have been specially selected to take advantage of this fantastic limited offer:</p>';?>
 <?php endif; ?>
 <p class="big">40% off <br>your next visit</p>
-<p class="small">before 29/11/13</p>
+<p class="small">to be taken before 29/11/13</p>
 <p>PLUS the price on your following visits will be fixed at:</p>
 
 <?php if($model->gender == 'm'): ?>
 	
-	<p>Just &pound;20 for a men's cut &amp; style</p>
+	<p><strong>Just &pound;20 for a men's cut &amp; style</strong></p>
 
 <?php else: ?>
 
 	<?php if ($model->last_stylist == 'Megan Mullaney' or $model->last_stylist == 'Natasha Bailey' or 
 	$model->last_stylist == 'Jack Broady' or $model->last_stylist == 'Leon Pritchard' or $model->last_stylist == 'Nathan Hinds'): ?>
 	
-	<p>Colour &amp; Cut package: £65<br>
-	Cut, Dry &amp; Style : £30<br>
-	Dry &amp; Style : £20</p>
+	<p><strong>Colour &amp; Cut package: &pound;65<br>
+		Cut, Dry &amp; Style : &pound;30<br>
+		Dry &amp; Style : &pound;20</strong></p>
 	<p class="small">With Meg, Tash, Jack or Leon</p>
 	
 	<?php else: ?>
 	
-	<p>Colour &amp; Cut package: £80<br>
-	Cut, Dry &amp; Style : £35<br>
-	Dry &amp; Style : £25</p>
+	<p><strong>Colour &amp; Cut package: &pound;80<br>
+		Cut, Dry &amp; Style : &pound;35<br>
+		Dry &amp; Style : &pound;25</strong></p>
 	<p class="small">With Kellie, Izzy, Jo, Michelle or Kate</p>
 	
 	<?php endif; ?> <!--end stylist-->
 	
 <?php endif; ?> <!--end gender-->
+<p>Until March 2014!</p>
+<small>Not with any other offer, subject to availability</small>
 
 </div> <!--#limited_copy-->
 </div> <!--#limited-->
