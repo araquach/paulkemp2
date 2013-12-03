0bcc1d88492d2118baaa0cbdd908c9f00cd4027e
Araquach <adam@contra-design.com>
2013-12-03 13:45:45 +0000

    getting going on Aveda comp

--- protected/views/competition/create.php
+++ protected/views/competition/create.php
@@ -1,11 +1,11 @@
 <?php
 /* @var $this CompetitionController */
 /* @var $model Competition */
-$this->pageTitle=Yii::app()->name . ' - Win ghd Candy Collection irons - Hairdressers in Cheshire';
+$this->pageTitle=Yii::app()->name . ' - WIN an Aveda in-salon experience';
 ?>
 
-<div id="overlay"></div> <!--competition closed overlay-->
-<section id="competition">
+<!--<div id="overlay"></div> 
+<section id="competition">-->
 
 
 <?php if(Yii::app()->user->hasFlash('entry')): ?>
@@ -20,19 +20,16 @@ $this->pageTitle=Yii::app()->name . ' - Win ghd Candy Collection irons - Hairdre
 <div id="competition_copy">
 <!--<p class="big comp_end">The competition is now closed - look out for another one soon!</p>-->
 
-<h1>WIN</h1>
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/ghdlogo.png" alt="ghd" width="190" height="129"/>
-<p class="candy">Candy Collection<br>Styling Irons</p>
-<p class="ghdtext"><span class="colours">mints, lemons &amp; violets have more fun</span><br>
-Your styler, just like your hair colour, should be an expression of you. Show off your personality with a new styler from ghd IV candy collection in violet, mint or lemon. When the others see how glossy your hair is you'll leave them green with envy.
-<br><strong>ghd IV candy collection is available in the salon now</strong></p>
-<p>For your chance to win a set of these sought after, limited edition ghd's just answer the following question:</p>
-<p class="question">How many colour options are available in the ghd Candy Collection range?</p>
+<h1>WIN AN</h1>
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/aveda_logo.png" alt="ghd" width="300" height="100"/>
+<h2>In-salon Experience</h2>
+<h3>worth over &pound;150!</h3>
+
+<p class="question">Aveda use a very special hand made paper for the packaging of their Christmas gift sets. Where is this paper made?</p>
 
 
-<?php  //echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php  echo $this->renderPartial('_form', array('model'=>$model)); ?>
 </div> <!--end #competition_copy-->
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/competition/comp_form.gif" alt="competition form" />
-</section> <!--end #compettion-->
+</section> <!--end #competion-->
 
 <?php endif ?>
