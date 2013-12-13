017a3dcb545d6deb7e62744bdcd85496e87875f0
Araquach <adam@contra-design.com>
2013-12-13 17:46:47 +0000

    updated the offers

--- protected/views/site/pages/offersmen.php
+++ protected/views/site/pages/offersmen.php
@@ -3,10 +3,10 @@
 $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 ?>
 
-<?php Yii::app()->facebook->ogTags['og:title'] = "Last Minute Blow Dry"; ?>
+<?php Yii::app()->facebook->ogTags['og:title'] = "Last Minute Men's Cut"; ?>
 <?php Yii::app()->facebook->ogTags['og:description'] = "Call on the day and if we have availability we'll do your hair at a special price:
-£10 Blowdry £15 Curly Blow"; ?>
-<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/last_min_meta.jpg"; ?>
+£12 Men's Cut & Style £7 Men's Express Cut"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/last_min_man_meta.jpg"; ?>
 
 <section id="offersMain">
 
@@ -15,18 +15,10 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 <section id="offer_copy">
 <p><strong>The PK team are pretty much booked up by now with limited appointment slots available. At this time of year we increase our opening days with the salon open on the Sunday's and Monday's leading up to Christmas.</strong></p>
 <p>If you still haven't booked your appointment yet - don't worry, we might still be able to get you in. We get a lot of last minute cancellations this time of year leaving spaces we'd really like to fill!</p>
-<p><strong>Call on the day and if we have availability we'll do your hair at a special price!</strong></p>
+<p><strong>Call on the day and if we have availability we'll do your hair at a special price! We can't guarantee availability and you can't choose your stylist, but you will have a fully qualified stylist or senior stylist.</strong></p>
 </section>
 
 	<section id="offerlist">
-	<div id="offer1" class="offer">
-	  <h3>Last Minute Blow Dry</h3>
-	  <p>Call on the day - if we have availability:</p>
-	  <p>&pound;10 Blow Dry</p>
-	  <p>&pound;15 Curly Blow</p>
-	    <p class="small">Until 31/12/13 Subject to availability</p>
-	</div>
-	
 	<div id="offer2" class="offer">
 	  <h3>Last Minute Men's Cut</h3>
 	 <p>Call on the day - if we have availability:</p>
