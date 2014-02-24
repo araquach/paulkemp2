50bea83390da365d09086b043043e3fcab3702e8
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-02-24 11:13:56 +0000

    changed offers page

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -9,65 +9,9 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 
 <section id="offersMain">
 
-<!--<h2>Offers</h2>
-
-<section id="offer_copy">
-<p><strong>The PK team are pretty much booked up by now with limited appointment slots available. At this time of year we increase our opening days with the salon open on the Sunday's and Monday's leading up to Christmas.</strong></p>
-<p>If you still haven't booked your appointment yet - don't worry, we might still be able to get you in. We get a lot of last minute cancellations this time of year leaving spaces we'd really like to fill!</p>
-<p><strong>Call on the day and if we have availability we'll do your hair at a special price! We can't guarantee availability and you can't choose your stylist, but you will have a fully qualified stylist or senior stylist.</strong></p>
-</section>
-
-	<section id="offerlist">
-	<div id="offer1" class="offer">
-	  <h3>Last Minute Blow Dry</h3>
-	  <p>Call on the day - if we have availability:</p>
-	  <p>&pound;10 Blow Dry</p>
-	  <p>&pound;15 Curly Blow</p>
-	    <p class="small">Until 31/12/13 Subject to availability</p>
-	</div>
+<h2>Offers</h2>
 	
-	<div id="offer2" class="offer">
-	  <h3>Last Minute Men's Cut</h3>
-	 <p>Call on the day - if we have availability:</p>
-	 <p>&pound;12 Men's Cut &amp; Style</p>
-	 <p>&pound;7 Men's Express Cut</p>
-	   <p class="small">Until 31/12/13 Subject to availability</p>
-	</div>
-	</section> <!- -offerlist- -*> 
-
-</section> <!- -#offersMain- -*>-->
-
-<!--<div id="offer1" class="offer"><h3>Senior Stylist Colour &amp; Cut Package</h3>
-<p>for just &pound;60</p>
-<p>with any of our Senior Stylists</p>
-<p class="small">offer until <time datetime="2013-08-30">30/08/13</time><br>
-  For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
-</div>-->
-
-<!--<div id="offer2" class="offer">
-  <h3>Stylist Colour &amp; Cut Package</h3>
-  <p>for just &pound;40<br>
-    <p>with any of our Stylists</p>
-    <p class="small">offer until <time datetime="2013-08-30">30/08/13</time><br>
-      For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
- </div>-->
-  
-<?php /* echo CHtml::link('<div id="offer4" class="offer">
-  <h3>FREE Cut, Dry &amp; Style</h3>
-  <p>with every Kebelo System Treatment</p><p class="small">until <time datetime="2013-08-30">30/08/13</time><br>
-    not with any other offer<br>Excludes Saturday</p>
- </div>', array('kebelo/advantage')); */ ?>
-
  
- 
- <section id="no_offer">
-  </section> 
- <section id="no_offercopy">
- <h2>Special Offers</h2>
- <p>Look out for some great offers coming soon!</p>
- <p>
- 	<a href="http://www.facebook.com/PaulKempHairdressing" target="_blank">Visit our Facebook page to keep up to date with all the latest news and offers</a>
- </p>
- </section> <!--no_offer-->
 
- 
+</section> <!--#offersMain-->
+
