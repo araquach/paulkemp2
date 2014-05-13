da1b8ebd6ae45012045c473e794c11e6b45d9c48
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-05-13 15:14:41 +0100

    updated offers for May

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -4,18 +4,26 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 ?>
 
 <?php Yii::app()->facebook->ogTags['og:title'] = "Offers"; ?>
-<?php Yii::app()->facebook->ogTags['og:description'] = ""; ?>
-<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/.jpg"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "All the latest offers at PK"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/last_min_meta.jpg"; ?>
 
 <section id="offersMain">
 
-<section id="no_offer">
-  </section> 
- <section id="no_offercopy">
- <h2>Special Offers</h2>
- <p>Look out for some great offers coming soon!</p>
- <p>
- 	<a href="http://www.facebook.com/PaulKempHairdressing" target="_blank">Visit our Facebook page to keep up to date with all the latest news and offers</a>
- </p>
-</section> <!--no_offer-->
+<h2>Offers</h2>
+
+<div id="offer1" class="offer">
+<h3>FREE Colour <br>with every<br>Cut, Dry &amp; Style</h3>
+<p>For new clients<br>until the end of May</p>
+<p class="small">offer until <time datetime="2014-05-30">30/05/14</time><br>
+  For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
+</div>
+  
+<?php  echo CHtml::link('<div id="offer4" class="offer">
+  <h3>FREE Cut, Dry &amp; Style</h3>
+  <p>with every Kebelo System Treatment</p><p class="small">until <time datetime="2014-05-30">30/05/14</time><br>
+    not with any other offer<br>Excludes Saturday</p>
+ </div>', array('kebelo/advantage')); ?>
+ 
+ </section> <!--end offersMain-->
+
 
