639e951185d057be72083d29eb27c02d8ef3b8d1
Araquach <adam@contra-design.com>
2014-12-09 18:03:03 +0000

    moved the copy about the salon

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -9,25 +9,15 @@ $this->pageTitle=Yii::app()->name;
 	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/scripts/feedback-feed.js');
 ?>
 
-<section id="homeimage">
-<div id="feedback_feed">
-	<ul>
-	<?php foreach ($model as $key => $value): ?>
-		<?php echo '<li>&quot;'. $value->extra_edit . '&quot;<br>' . '<span class="client">' . ucfirst($value->FeedbackClient->first_name) . ' ' . ucfirst($value->FeedbackClient->last_name) . ' - hair by ' . strstr($value->FeedbackClient->stylist, ' ', true) . '</span></li>'?>
-	
-	<?php endforeach; ?>
-	</ul>
-</div> <!--#feedback_feed-->
-
-    </section>
-    <section id="homecopy">
+<section id="homeimage"></section>
+    
+ <section id="homecopy">
     <h2>A New Standard of Hairdressing</h2>
     <p>Paul Kemp Hairdressing is located right in the heart of Warrington town centre. Sister salon to the award winning <em><a href="http://www.jakatasalon.co.uk/" target="_blank">Jakata</a></em>, the stunning salon opened in June 2011 with the aim to offer an ultra relaxing atmosphere, first class customer service, alongside the highest level of hairdressing expertise.</p>
     <p>The talented hairdressing team are all trained to the highest level in cutting, colouring and styling hair, with specialists in technical colour, hair straightening, wedding hair and hair extensions. The team has a wealth of experience in all aspects of hairdressing so you can just sit back, relax and enjoy the experience.</p>
     <p><em>The salon has built up a strong reputation for creating stunning, fashionable looks tailored to suit you, and your lifestyle, so if you're looking for a new <strong class="paraend">hairdressers in Warrington</strong> then look no further!</em></p>
-    
-	</section>
-  
+ </section>
+	
 <!--NewsItems-->
 
 	<?php echo CHtml::link('<aside id="news1">
@@ -55,5 +45,11 @@ $this->pageTitle=Yii::app()->name;
     </aside>',array('site/page','view'=>'offers')); ?>
     
     
-    
-    
+  <div id="feedback_feed">
+  	<ul>
+  	<?php foreach ($model as $key => $value): ?>
+  		<?php echo '<li>&quot;'. $value->extra_edit . '&quot;<br>' . '<span class="client">' . ucfirst($value->FeedbackClient->first_name) . ' ' . ucfirst($value->FeedbackClient->last_name) . ' - hair by ' . strstr($value->FeedbackClient->stylist, ' ', true) . '</span></li>'?>
+  	
+  	<?php endforeach; ?>
+  	</ul>
+  </div> <!--#feedback_feed-->
