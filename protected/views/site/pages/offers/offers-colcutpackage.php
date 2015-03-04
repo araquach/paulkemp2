d5874e3d747d690ec7a5140d587b27a6fb79cf5b
Araquach <adam@contra-design.com>
2015-03-04 13:19:05 +0000

    removed the offers

--- protected/views/site/pages/offers/offers-colcutpackage.php
+++ protected/views/site/pages/offers/offers-colcutpackage.php
@@ -3,37 +3,45 @@
 $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 ?>
 
-<?php Yii::app()->facebook->ogTags['og:title'] = "Special September Offers"; ?>
-<?php Yii::app()->facebook->ogTags['og:description'] = "Colour & Cut for just £70 with Senior Stylists, £50 with Stylists for new clients"; ?>
-<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/metalixx_meta.jpg"; ?>
+<?php Yii::app()->facebook->ogTags['og:title'] = "February Special Offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Colour & Cut for just £80 with Senior Stylists, £65 with Stylists for new clients throughot February"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/Asian_Lucy_123_meta.jpg"; ?>
 
 <section id="offersMain">
 
-<h2>Offers</h2>
+<h2>February Offers</h2>
 
 <section id="offerlist">
 
 <div id="offer1" class="offer"><h3>Senior Stylist Colour &amp; Cut Package</h3>
-<p>for just &pound;70</p>
+<p>for just &pound;80</p>
 <p>with any of our Senior Stylists</p>
-<p class="small">offer extended until <time datetime="2014-09-30">30/09/14</time><br>
+<p class="small">offer extended until <time datetime="2015-02-27">27/02/15</time><br>
   For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
 </div>
 
 <div id="offer2" class="offer">
   <h3>Stylist Colour &amp; Cut Package</h3>
-  <p>for just &pound;50<br>
+  <p>for just &pound;65<br>
     with any of our Stylists</p>
-    <p class="small">offer extended until <time datetime="2014-09-30">30/09/14</time><br>
+    <p class="small">offer until <time datetime="2015-02-27">27/02/15</time><br>
       For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
 </div>
 
 <div id="offer3" class="offer">
-  <h3>30% off</h3>
-  <p>Men's Cut &amp; Style<br>
-    with any of our Stylists/Senior Stylists</p>
-    <p class="small">offer extended until <time datetime="2014-09-30">30/09/14</time><br>
+  <h3>Men's Cut &amp; Style</h3>
+  <p>For &pound;16 with a Stylist<br>&pound;20 with a Senior Stylist<br></p>
+    <p class="small">until <time datetime="2015-02-27">27/02/15</time><br>
       For New Clients Only - not with any other offer - excludes Saturday</p>
+</div>
+
+<div id="fb_like">
+<?php  $this->widget('ext.yii-facebook-opengraph.plugins.LikeButton', array(
+   'href' => 'http://www.paulkemphairdressing.com/offers', 
+   'show_faces'=>false,
+   'send' => true,
+   'width' => '250px'
+));  ?>
 </div>
 
 
