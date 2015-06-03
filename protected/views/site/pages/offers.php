d480031fde8c78c84360ba4ce95d346c87cf72b4
Araquach <adam@contra-design.com>
2015-06-03 17:26:23 +0100

    extended the offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -3,8 +3,8 @@
 $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 ?>
 
-<?php Yii::app()->facebook->ogTags['og:title'] = "New Client offers for Spring"; ?>
-<?php Yii::app()->facebook->ogTags['og:description'] = "1/2 Price Colour with every Cut, Dry & Style for new clients throughout May"; ?>
+<?php Yii::app()->facebook->ogTags['og:title'] = "New Client offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "1/2 Price Colour with every Cut, Dry & Style for new clients extended throughout June"; ?>
 <?php Yii::app()->facebook->ogTags['og:image'] = "http://www.paulkemphairdressing.com/images/offer/katharina_meta.jpg"; ?>
 
 <section id="offersMain">
@@ -17,14 +17,14 @@ $this->pageTitle=Yii::app()->name . ' - Offers - Hairdressers in Cheshire';
 <div id="offer1" class="offer">
 <h3>1/2 Price Colour with every<br>Cut, Dry &amp; Style</h3>
 <p>For new clients<br>until the end of May</p>
-<p class="small">offer until <time datetime="2015-05-29">29/05/14</time><br>
+<p class="small">offer until <time datetime="2015-06-30">30/06/15</time><br>
   For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
 </div>
 
 <div id="offer3" class="offer">
   <h3>Men's Cut &amp; Style</h3>
   <p>For &pound;16 with a Stylist<br>&pound;20 with a Senior Stylist<br></p>
-    <p class="small">until <time datetime="2015-05-29">29/05/15</time><br>
+    <p class="small">until <time datetime="2015-06-30">30/06/15</time><br>
       For New Clients Only - not with any other offer - excludes Saturday</p>
 </div>
  
