5b595252ec00257674e38120eb1955f0e3838b48
Araquach <adam@contra-design.com>
2015-10-13 11:40:44 +0100

    removed offer link

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -33,10 +33,10 @@ $this->pageTitle=Yii::app()->name;
 		</aside>',array('kebelo/index')); ?>
 	
 	<?php echo CHtml::link('<aside id="news3">
-    <h3 class="newsheader">This month\'s<br>Special Offer\'s</h3>
-    <p class="newspara">New to the salon? Check out these Summer Offers</p>
+    <h3 class="newsheader">Fibre Force<br>Is Here</h3>
+    <p class="newspara">A new dimension of hair repair</p>
     <p class="newscenter"><strong>Find out more here &gt;</strong></p>
-    </aside>',array('site/page','view'=>'offers')); ?>
+    </aside>',array('site/page','news', '#'=>'FibreForce')); ?>
     
     <?php echo CHtml::link('<aside id="news4">
     <h3 class="newsheader">Great Career Opportunities</h3>
